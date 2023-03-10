5d7825a1787a207e1eea319cc2a179c2908d2d97
Anna Kors <ankors320@gmail.com>
2023-03-11 01:36:25 +0300

    Done task4

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-ое число: ");
+int k = Convert.ToInt32(Console.ReadLine());
+int max = n;
+
+if (m > max) max = m;
+if (k > max) max = k;
+	Console.WriteLine(max);
