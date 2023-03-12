76371248c85f257ae9d28ec1a85caa264813d66b
Anna Kors <ankors320@gmail.com>
2023-03-12 18:24:19 +0300

    Done

---
+++ Task15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите день недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись!\nВведите день недели: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n > 5)
+    Console.WriteLine("Да");
+else 
+        Console.WriteLine("Нет");    
