4be136e6019c257f5553aa2d0b8501ace61e8087
Anna Kors <ankors320@gmail.com>
2023-03-12 17:07:15 +0300

    Done

---
+++ Task10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 99 || n > 1000)
+{
+	Console.WriteLine("Вы ошиблись!\nВведите трехзначное число: ");
+	n = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine((n / 10) % 10);
+
