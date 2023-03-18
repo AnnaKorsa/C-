c165fa567ed59d22496e2a61a77c78db667a9acd
Anna Kors <ankors320@gmail.com>
2023-03-19 00:15:19 +0300

    Done

---
+++ Task19/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int temp = n;
+int res = 0;
+while (n > 0)
+{
+    res = res * 10 + n % 10;
+	n = n / 10;
+}
+if(temp == res)
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");
