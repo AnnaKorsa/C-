bb2d3507d264c54632d6947cec62ee05ccb84896
Anna Kors <ankors320@gmail.com>
2023-03-20 21:41:05 +0300

    Done

---
+++ Task27/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int i = 0;
+while ( n != 0 )
+{
+sum = sum + n % 10;
+n = n / 10;
+i++;
+}
+Console.WriteLine(sum);
