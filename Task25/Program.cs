b99006c9a687de91faae566d4af9fe3f2467a355
Anna Kors <ankors320@gmail.com>
2023-03-20 21:18:05 +0300

    Done

---
+++ Task25/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число A: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int exponent = n;
+for (int i = 1;i < m; i++)
+	exponent = exponent * n;
+Console.WriteLine(exponent);
