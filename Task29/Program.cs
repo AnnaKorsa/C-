7530d0b2868c398f9afc141aa4150315001fdd39
Anna Kors <ankors320@gmail.com>
2023-03-22 20:43:35 +0300

    Done

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -3,3 +3,4 @@ int[] array = new int[8];
 for (int i = 0; i < array.Length; i++)
     array[i] = new Random().Next(1, 11); // [1, 10]
 Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+  
