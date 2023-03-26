3a25644f41134b27272f3709180de5471dd1a7a4
Anna Kors <ankors320@gmail.com>
2023-03-26 16:00:56 +0300

    Done1

---
+++ task/Task41/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите количество чисел: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите числа: ");
+int[] array = Console.ReadLine()!.Split().Select(i => int.Parse(i)).ToArray();
+int count = 0;
+foreach (int element in array)
+{
+    if (element > 0)
+        count++;
+}
+Console.WriteLine(count);
