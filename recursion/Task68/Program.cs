0af8cd2d410e871c08e7889f0eef5ca7abd75443
Anna Kors <ankors320@gmail.com>
2023-04-09 11:23:31 +0300

    Done

---
+++ recursion/Task68/Program.cs
@@ -0,0 +1,22 @@
+﻿int f(int m, int n)
+{
+    if (m == 0) 
+	{
+    	return n + 1;
+	}
+	else if (n == 0 && m > 0) 
+	{
+        return f(m - 1, 1);
+	}
+    else 
+	{
+    	return f(m - 1, f(m, n - 1));
+	}
+}
+
+Console.Clear();
+Console.Write("Введите начальное число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите конечное число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(m, n));
